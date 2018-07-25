14e30e40fa596e4379fcb2c8a29cbf5351955277
Martin <martin.hloska@gmail.com>
2018-07-25 12:37:45 +0200

    remove views, left only mixin

--- tests/testapp/views.py
+++ tests/testapp/views.py
@@ -1,16 +1,16 @@
 from rest_framework import generics
 
-from rest_framework_twoways.views import TwoWayAPiView
+from rest_framework_twoways.mixins import  TwoWaySerializerMixin
 from tests.testapp.models import Post
 from tests.testapp.serializers import PostInputSerializer, PostOutputSerializer
 
 
-class PostListCreateView(TwoWayAPiView, generics.ListCreateAPIView):
+class PostListCreateView(TwoWaySerializerMixin, generics.ListCreateAPIView):
     input_serializer_class = PostInputSerializer
     output_serializer_class = PostOutputSerializer
 
     queryset = Post.objects.all()
 
 
-class SerializerNotSetView(TwoWayAPiView, generics.ListCreateAPIView):
+class SerializerNotSetView(TwoWaySerializerMixin, generics.ListCreateAPIView):
     queryset = Post.objects.all()
